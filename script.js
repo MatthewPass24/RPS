9ba74e3c338618924a336c8f50a960c0398a12f3
MatthewPass24 <matthewpass24@gmail.com>
2024-02-22 10:44:06 -0500

    added footer

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 let name = prompt('Enter your name')
+ket
 let message = `Congratulations, ${name} you won the game.`
+
+let objArray = ["rock", "paper", "scissors",]
+function playRound(userSelection) {
+    const computerChoice = Math.floor(Math.random() * objArray.length )
+}
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
