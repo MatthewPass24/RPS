104b1519b6dc09c4b9da50ffe044ff1be4bb7a69
MatthewPass24 <matthewpass24@gmail.com>
2024-02-23 11:54:11 -0500

    finalized name prompt and started rounds js

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-let name = prompt('Enter your name')
-document.getElementById('response-text').innerText = `${name}`
 
-if (name === null) {
-  document.getElementById('response-text').innerText = 'Please ask a question'
-  document.getElementById('response-text').classList = 'Display text-danger'
-}
+window.onload = (event) => {
 
-if (!name.trim()) {
-  alert('Please enter a name!')
-  return
-}
-let message = `Congratulations, ${name} you won the game.`
+  let userName = prompt('Enter your name')
+
+
+  if (userName === null) {
+    document.getElementById('response-text').innerText = 'Please enter your name'
+    document.getElementById('response-text').classList = 'Display text-light h3'
+  }
+  
+  if (!userName.trim()) {
+    alert('Please enter a name!')
+    userName = prompt('Enter your name')
+  }
+  document.getElementById('response-text').innerText = `${userName}`
+};
+
+
+let message = `Congratulations, ${userName} you won the game.`
 
 let objArray = ["rock", "paper", "scissors",]
+
 function playRound(userSelection) {
-  const computerChoice = Math.floor(Math.random() * objArray.length)
+  const computerChoice = objArray[Math.floor(Math.random() * 3)]
+  let result = "";
+
+  if(userSelection === computerChoice){
+    result = "It's a tie!"
+  }
+
+  else{
+    switch(userSelection){
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  }
 }
+  
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
