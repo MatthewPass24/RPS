cebdc7d124538bb57e85ca501608279eb2a468ea
MatthewPass24 <matthewpass24@gmail.com>
2024-02-22 21:40:04 -0500

    added name function

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 let name = prompt('Enter your name')
-ket
+document.getElementById('response-text').innerText = `${name}`
+
+if (name === null) {
+  document.getElementById('response-text').innerText = 'Please ask a question'
+  document.getElementById('response-text').classList = 'Display text-danger'
+}
+
+if (!name.trim()) {
+  alert('Please enter a name!')
+  return
+}
 let message = `Congratulations, ${name} you won the game.`
 
 let objArray = ["rock", "paper", "scissors",]
 function playRound(userSelection) {
-    const computerChoice = Math.floor(Math.random() * objArray.length )
+  const computerChoice = Math.floor(Math.random() * objArray.length)
 }
 const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-      case 0:
-        return 'rock';
-      case 1:
-        return 'paper';
-      case 2:
-        return 'scissors';
-    }
-  };
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
